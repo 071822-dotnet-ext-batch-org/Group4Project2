ebe3725d6ac4643c8bfa6c9dfe9a240a9c799fb6
alicia984@revature.net <alicia984@revature.net>
2022-08-27 14:56:43 -0500

    On branch apiupdatesAlicia  Changes to be committed: 	modified:   OurBooksAPI/BusinessLayer/Business.cs 	modified:   OurBooksAPI/BusinessLayer/BusinessLayer.csproj     added: added item group to make project build and run
    	new file:   OurBooksAPI/ModelsLayer/Credentials.cs
        Titled file Credentials to make it less confusing when referring to the login
    	modified:   OurBooksAPI/ModelsLayer/ModelsLayer.csproj
        added: added item group to make project build and run
    
    	modified:   OurBooksAPI/OurBooksAPI/Controllers/Controller.cs
    
    	modified:   OurBooksAPI/RepositoryLayer/Repository.cs
    	modified:   OurBooksAPI/RepositoryLayer/RepositoryLayer.csproj
        added: added item group to make project build and run

--- OurBooksAPI/BusinessLayer/Business.cs
+++ OurBooksAPI/BusinessLayer/Business.cs
@@ -1,7 +1,26 @@
-﻿namespace BusinessLayer
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+using ModelsLayer;
+using RepositoryLayer;
+
+namespace BusinessLayer
 {
     public class Business
     {
+        private Repository _repo = new Repository();
 
-    }
-}
+        private Credentials? _CurrentCredentials = null;
+
+        public async Task<bool> LoginAsync(string email, string password)
+        {
+            Credentials? c = await this._repo.GetCredentialsAsync(email, password);
+            if (c != null && c.Email == email && c.Password == password) //If useer is in directory with this email and password
+            {
+                _CurrentCredentials = c;
+                return true;
+            }
+            return false;
+        }//EoLoginAsync
+
+    }//EoC
+}//EoN
