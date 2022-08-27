2241842335223cbd63c98fb5e3fc74d287f81f39
Larry Pallozzi <109549983+LarryPallozziRevature1@users.noreply.github.com>
2022-08-27 19:24:46 -0400

    API project 2

--- OurBooksAPI/BusinessLayer/Business.cs
+++ OurBooksAPI/BusinessLayer/Business.cs
@@ -1,7 +1,35 @@
-﻿namespace BusinessLayer
-{
-    public class Business
-    {
+﻿using ModelsLayer;
+using RepositoryLayer;
 
-    }
-}
+namespace BusinessLayer
+{
+    public class Business 
+    {
+        private Repository _repo = new Repository();
+        /*
+        private readonly Repository _repositoryLayer; // Private field for Business constructor
+        public Business() // Constructor linking to the RepositoryLayer
+        {
+            this._repositoryLayer = new Repository();
+        }
+        */
+        /// <summary>
+        /// #3 Display products
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="BookName"></param>
+        /// <param name="NumberPages"></param>
+        /// <param name="Genre"></param>
+        /// <param name="Author"></param>
+        /// <param name="InStock"></param>
+        /// <param name="Cost"></param>
+        /// <returns></returns>
+        public async Task<List<DisplayDTO>> DisplayProductAsync(string? ProductId, string? BookName, int? NumberPages, string? Genre, string? Author, int? InStock, decimal? Cost)
+        {   // create list result from repo layer
+            List<DisplayDTO> result = await this._repo.DisplayProductAsync(ProductId, BookName, NumberPages, Genre, Author, InStock, Cost);
+            return result; // Return 
+        }
+
+
+    } //EC
+} //EN
--- OurBooksAPI/OurBooksAPI/Controllers/Controller.cs
+++ OurBooksAPI/OurBooksAPI/Controllers/Controller.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BusinessLayer;
+using ModelsLayer;
+using System.Reflection.Metadata.Ecma335;
+using RepositoryLayer;
 
 namespace OurBooksAPI.Controllers
 {
@@ -7,5 +11,44 @@ namespace OurBooksAPI.Controllers
     [ApiController]
     public class Controller : ControllerBase
     {
-    }
-}
+        private Business _business = new Business(); // Creating a new business connection object
+
+        /*
+        private readonly Business _businessLayer; //Private field to be used in constructor for Controller().
+        public Controller() //Constructor to connect to BusinessLayer.
+        {
+            this._businessLayer = new Business();
+        }
+        */
+
+        /// <summary>
+        /// #3 Display products
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <param name="BookName"></param>
+        /// <param name="NumberPages"></param>
+        /// <param name="Genre"></param>
+        /// <param name="Author"></param>
+        /// <param name="InStock"></param>
+        /// <param name="Cost"></param>
+        /// <returns></returns>
+        [HttpGet("Display_Product")] //create GET request in API
+        public async Task<ActionResult<List<DisplayDTO>>> DisplayProductAsync(string? ProductId, string? BookName, int? NumberPages, string? Genre, string? Author, int? InStock, decimal? Cost)
+        {   // send list to the API
+            List<DisplayDTO> result = await this._business.DisplayProductAsync(ProductId, BookName, NumberPages, Genre, Author, InStock, Cost);
+            if (ModelState.IsValid)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+
+
+
+    }//EC
+} //EN
