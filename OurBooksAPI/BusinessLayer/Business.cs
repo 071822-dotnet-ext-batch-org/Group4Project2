55273bd5d238a402c18478ce9c0cebdc08f305e9
Beverly Johnson <beverly485@revature.net>
2022-08-28 21:29:43 -0400

    adds register code

--- OurBooksAPI/BusinessLayer/Business.cs
+++ OurBooksAPI/BusinessLayer/Business.cs
@@ -1,7 +1,26 @@
-﻿namespace BusinessLayer
+﻿using ModelsLayer;
+using RepositoryLayer;
+
+
+namespace BusinessLayer
 {
     public class Business
     {
+        private readonly Repository _repo; 
+        public Business() 
+        {
+            this._repo = new Repository();
+        }
+        //private Repository _repo = new Repository();
+
+        //method to register a new account and input new customer info
+        public async Task<RegisterAccount> RegisterAccountAsync(Guid userId, string? firstName, string? lastName, string? deliveryAddress, int phone, string? email, string? isAdmin)
+        {
+            RegisterAccount customerInfo = await this._repo.RegisterAccountAsync(userId, firstName, lastName, deliveryAddress, phone, email, isAdmin);
+            return customerInfo;
+            
+        }
+       
 
     }
 }
--- OurBooksAPI/OurBooksAPI/Controllers/Controller.cs
+++ OurBooksAPI/OurBooksAPI/Controllers/Controller.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ModelsLayer;
 
 namespace OurBooksAPI.Controllers
 {
@@ -7,5 +16,30 @@ namespace OurBooksAPI.Controllers
     [ApiController]
     public class Controller : ControllerBase
     {
+        private readonly Business _business; 
+        public Controller() 
+        {
+            this._business = new Business();
+        }
+
+        //private Business _business = new Business();
+
+        //Register a new account with username and password 
+        [HttpPost("RegisterAccount")]
+        //add new customer accounts to the database
+ 
+        public async Task<ActionResult<RegisterAccount>> RegisterAccountAsync(Guid userId, string? firstName, string? lastName, string? deliveryAddress, int phone, string? email, string? isAdmin)
+        {
+
+            RegisterAccount customerInfo = await this._business.RegisterAccountAsync(userId, firstName, lastName, deliveryAddress, phone, email, isAdmin);
+            return Ok();
+
+        }
+
+
+
     }
+
+
+
 }
